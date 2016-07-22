f2df081677770cd4beb310acca7116773f89fe9b
Halima KOUNDI <hkoundi@gmail.com>
2016-07-22 15:55:15 +0100

    Add project files.

---
+++ CQRS_Session1.Src/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS_Session1.Src
+{
+    public class Class1
+    {
+    }
+}
