35f94d575662c00dcb5a1912979c49d23775e919
Halima KOUNDI <hkoundi@gmail.com>
2016-07-22 16:25:09 +0100

    CRUD code

---
+++ CQRS_Session1.Tests/UserRepositoryShould.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using CQRS_Session1.Src;
+using NUnit.Framework;
+
+namespace CQRS_Session1.Tests
+{
+    [TestFixture]
+    public class UserRepositoryShould
+    {
+        [Test]
+        public void create_a_user()
+        {
+            var expectedUsers = new List<User>() {new User("User Name")};
+            var userRepo    = new UserRepository();
+
+            userRepo.CreateUser("User Name");
+
+            var users   = userRepo.AllUsers();
+            Assert.That(users, Is.EqualTo(expectedUsers));
+        }
+
+        [Test]
+        public void find_user_by_name()
+        {
+            var userName = "User Name";
+            var user    = new User(userName);
+            var userRepo    = new UserRepository();
+            userRepo.CreateUser(userName);
+
+            var retrievedUser   = userRepo.FindByName(userName);
+
+            Assert.That(retrievedUser, Is.EqualTo(user));
+        }
+    }
+}
